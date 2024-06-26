39a948c326073f828c63274314e959d289f29714
Aleksi Berishvili <aberi21@freeuni.edu.ge>
2024-06-27 00:37:42 +0400

    Adding Front
    adding simple front to display books and members tables on web

--- src/main/kotlin/org/abel/plugins/Routing.kt
+++ src/main/kotlin/org/abel/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package org.abel.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.response.*
@@ -11,32 +12,36 @@ fun Application.configureRouting(library: LibraryStorage) {
     routing {
         staticResources("static", "static")
 
-        get("/library") {
-            call.respondText("Hello World!")
-        }
         get("/library/get-members") {
             call.respond(library.getMembers())
-
         }
+
         get("/library/get-books") {
             call.respond(library.getBooks())
+        }
 
-        }
         get("/library/get-member") {
-            call.respond(
-                library.findMemberById(
-                    (call.request.queryParameters["memberId"]
-                        ?: throw IllegalArgumentException("Parameter memberId not found")).toInt()
-                )
-            )
+            try {
+                val memberId = (call.request.queryParameters["memberId"] ?: throw IllegalArgumentException()).toInt()
+                if (memberId < 0) throw NumberFormatException()
+                call.respond(library.findMemberById(memberId))
+            } catch (_: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest, "Parameter 'memberId' not found")
+            } catch (_: NumberFormatException) {
+                call.respond(HttpStatusCode.BadRequest, "Parameter 'memberId' must be a positive Integer")
+            }
         }
+
         get("/library/get-book") {
-            call.respond(
-                library.findBookById(
-                    (call.request.queryParameters["bookId"]
-                        ?: throw IllegalArgumentException("Parameter bookId not found")).toInt()
-                )
-            )
+            try {
+                val bookId = (call.request.queryParameters["bookId"] ?: throw IllegalArgumentException()).toInt()
+                if (bookId < 0) throw NumberFormatException()
+                call.respond(library.findBookById(bookId))
+            } catch (_: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest, "Parameter 'bookId' not found")
+            } catch (_: NumberFormatException) {
+                call.respond(HttpStatusCode.BadRequest, "Parameter 'bookId' must be a positive Integer")
+            }
         }
     }
 }
